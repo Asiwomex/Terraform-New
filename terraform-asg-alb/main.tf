0abc1990193cdec2e90b43c8514d83d2bb011b3c
Asiwomex <118656806+Asiwomex@users.noreply.github.com>
2024-10-14 11:30:35 +0000

    added subnet to asg-alb.tf

--- terraform-asg-alb/main.tf
+++ terraform-asg-alb/main.tf
@@ -12,3 +12,13 @@ resource "aws_vpc" "my_vpc" {
     Name = "MyASGVPC"
   }
 }
+# Subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "MyPublicSubnet"
+  }
+}
