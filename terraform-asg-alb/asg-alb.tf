16fe2356bdd9ab1b0211d78aaab322c2574b92a0
Asiwomex <118656806+Asiwomex@users.noreply.github.com>
2024-10-14 11:34:06 +0000

    updated

--- terraform-asg-alb/asg-alb.tf
+++ terraform-asg-alb/asg-alb.tf
@@ -22,3 +22,156 @@ resource "aws_subnet" "my_subnet" {
     Name = "MyPublicSubnet"
   }
 }
+
+# Internet Gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "MyInternetGateway"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "MyRouteTable"
+  }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "my_route_table_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
+
+# Security Group for the ALB
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ALBSecurityGroup"
+  }
+}
+
+# Security Group for the EC2 Instances
+resource "aws_security_group" "instance_sg" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "InstanceSecurityGroup"
+  }
+}
+
+# Launch Template
+resource "aws_launch_template" "my_launch_template" {
+  name_prefix   = "my-launch-template"
+  image_id      = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 (Change as needed for your region)
+  instance_type = "t2.micro"
+  
+  security_group_names = [aws_security_group.instance_sg.name]
+
+  user_data = base64encode("#!/bin/bash\nyum update -y\nyum install -y httpd\nsystemctl start httpd\nsystemctl enable httpd\necho '<h1>Hello from ASG</h1>' > /var/www/html/index.html")
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
+
+# Auto Scaling Group
+resource "aws_autoscaling_group" "my_asg" {
+  desired_capacity     = 2
+  max_size             = 3
+  min_size             = 1
+  vpc_zone_identifier  = [aws_subnet.my_subnet.id]
+  launch_template {
+    id      = aws_launch_template.my_launch_template.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [aws_lb_target_group.my_target_group.arn]
+
+  tags = [
+    {
+      key                 = "Name"
+      value               = "MyASGInstance"
+      propagate_at_launch = true
+    }
+  ]
+}
+
+# ALB
+resource "aws_lb" "my_alb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.my_subnet.id]
+
+  tags = {
+    Name = "MyALB"
+  }
+}
+
+# Target Group
+resource "aws_lb_target_group" "my_target_group" {
+  name     = "my-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.my_vpc.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+}
+
+# ALB Listener
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.my_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_target_group.arn
+  }
+}
